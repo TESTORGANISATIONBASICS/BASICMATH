78ceadc5747b05c53ec0b3721a7f940ac4d09e1d
ARUN JOHNSON <arun.johnson@citta.ai>
2018-05-04 17:03:37 +0530

    INITIAL COMMIT

---
+++ src/com/basicmath/mathinterface/BasicMath.java
@@ -0,0 +1,16 @@
+package com.basicmath.mathinterface;
+
+public interface BasicMath {
+
+	void add(int a,int b);
+	void subtract(int a,int b);
+	void mutiply(int a,int b);
+	void divide(int a,int b);
+	void average(int[] a);
+	void mean(int[] a);
+	void mode(int[] a);
+	void areaOfCircle(int a);
+	void areaOfSquare(int a);
+	void areaOfRectangle(int a,int b);
+	
+}
